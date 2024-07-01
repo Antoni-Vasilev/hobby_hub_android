68b8fd0b2a65465ba467c6126ffc456a2cc5d9a1
toni <antonivasilev15@gmail.com>
2024-07-01 17:13:56 +0300

    1.2.1 - Добавен нов екран `RegisterActivity` - Направена връзка между екраните `LoginActivity` и `RegisterActivity` - Направен дизайн за екрана `RegisterActivity`

--- app/src/main/java/eu/tumara/hobby_hub/LoginActivity.kt
+++ app/src/main/java/eu/tumara/hobby_hub/LoginActivity.kt
@@ -1,12 +1,17 @@
 package eu.tumara.hobby_hub
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var btnRegisterNow: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +21,20 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
             insets
         }
+
+        init()
+    }
+
+    private fun init() {
+        btnRegisterNow = findViewById(R.id.btnRegisterNow)
+
+        btnRegisterNow.setOnClickListener {
+            showRegister()
+        }
+    }
+
+    private fun showRegister() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }
